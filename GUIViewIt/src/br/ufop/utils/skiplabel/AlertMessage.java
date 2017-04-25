d3035aa61d5fc487b2bb1984b09d4ecf2ad3b903
Marina <marinamsm@live.com>
2017-04-25 20:41:16 -0300

    AlertMessage is complete

--- GUIViewIt/src/br/ufop/utils/skiplabel/AlertMessage.java
+++ GUIViewIt/src/br/ufop/utils/skiplabel/AlertMessage.java
@@ -7,10 +7,25 @@ public class AlertMessage {
 	
 	public static void showConfirmationAlert(String title, String header, String content) {
 		Alert alert = new Alert(AlertType.CONFIRMATION);
+		showAlert(title, header, content, alert);
+	}
+	
+	public static void showErrorAlert(String title, String header, String content) {
+		Alert alert = new Alert(AlertType.ERROR);
+	}
+	
+	public static void showWarningAlert(String title, String header, String content) {
+		Alert alert = new Alert(AlertType.WARNING);
+	}
+	
+	public static void showInfoAlert(String title, String header, String content) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+	}
+	
+	private static void showAlert(String title, String header, String content, Alert alert) {
 		alert.setTitle(title);
 		alert.setHeaderText(header);
 		alert.setContentText(content);
 		alert.showAndWait();
 	}
-	
 }
